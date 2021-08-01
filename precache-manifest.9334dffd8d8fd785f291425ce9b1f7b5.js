cecdbdc8e0df6b80276c3ae6a732f21668267122
Roman <misteryo.wins@mail.ru>
2021-08-01 19:56:09 +0300

    Updates

---
+++ precache-manifest.9334dffd8d8fd785f291425ce9b1f7b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "254eaa23a92803c16765b4a318b2502c",
+    "url": "/todo-react/index.html"
+  },
+  {
+    "revision": "a4c39610c82d1ecbe6e4",
+    "url": "/todo-react/static/css/main.f4903a8f.chunk.css"
+  },
+  {
+    "revision": "f09b246faf15a1ff0037",
+    "url": "/todo-react/static/js/2.85a581b9.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/todo-react/static/js/2.85a581b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4c39610c82d1ecbe6e4",
+    "url": "/todo-react/static/js/main.99be2228.chunk.js"
+  },
+  {
+    "revision": "1614377f578425b5d648",
+    "url": "/todo-react/static/js/runtime-main.92cd31f8.js"
+  }
+]);
